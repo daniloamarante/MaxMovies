b997dc61e5cc810f059098ee016d73d7987c9429
Amarante <danilodesouza12@hotmail.com>
2020-07-28 10:35:44 -0300

    Setup inicial do Projeto

---
+++ src/components/Menu/components/ButtonLink/index.js
@@ -0,0 +1,16 @@
+import React from './node_modules/react';
+
+
+function ButtonLink(props){
+    //props => {}
+
+    return(
+        
+        <a href={props.href} className={props.className}>
+            {props.children}
+        </a>
+           
+    );
+}
+
+export default ButtonLink;
